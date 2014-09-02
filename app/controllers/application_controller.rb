25b701ad6ff52c2e05c15bd9a9a954db0b101f1d
Olivier Lance <olivier@digitalcuisine.fr>
2014-09-02 02:11:34 +0200

    Defined an `@application_state` instance variable on ApplicationController so that each controller's action can set the application's initial state for server rendering and client initialization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :init_application_state
+
+  protected
+  def init_application_state
+    @application_state = ActiveSupport::HashWithIndifferentAccess.new
+    @application_state[:path] = request.fullpath
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @application_state[:posts] = Post.all
   end
 
 end
